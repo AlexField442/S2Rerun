d4c4c0ba81308c7ef439de82aa22083d2ab2c8b9
AlexField442 <alexfield290@gmail.com>
2026-03-14 18:16:37 +1100

    Moved Obj27
    Moved to before Obj28 and Obj29 for organization reasons.

--- s2.asm
+++ s2.asm
@@ -24209,6 +24209,8 @@ Obj2D_MapUnc_11822:	include "mappings/sprite/obj2D.asm"
 
 	jmpTos JmpTo2_SolidObject
 
+; Obj27:
+	include	"_incObj/27 Explosions.asm"
 ; Obj28:
 	include	"_incObj/28 Animals.asm"
 ; Obj29:
@@ -29130,7 +29132,7 @@ ObjPtr_ARZBubbles:	dc.l Obj24	; Bubbles in Aquatic Ruin Zone
 ObjPtr_Ring:		dc.l Obj25	; A ring
 ObjPtr_Monitor:		dc.l Obj26	; Monitor
 ObjPtr_Explosion:	dc.l Obj27	; An explosion, giving off an animal and 100 points
-ObjPtr_Animal:		dc.l Obj28	; Animal and the 100 points from a badnik
+ObjPtr_Animal:		dc.l Obj28	; Animals
 ObjPtr_Points:		dc.l Obj29	; "100 points" text
 ObjPtr_Stomper:		dc.l Obj2A	; Stomper from MCZ
 ObjPtr_RisingPillar:	dc.l Obj2B	; Rising pillar from ARZ
@@ -45488,74 +45490,10 @@ Obj7C_Main:
 ; ---------------------------------------------------------------------------
 +	rts
 ; ===========================================================================
-; -------------------------------------------------------------------------------
+; ---------------------------------------------------------------------------
 ; sprite mappings
-; -------------------------------------------------------------------------------
+; ---------------------------------------------------------------------------
 Obj7C_MapUnc_2103C:	include "mappings/sprite/obj7C.asm"
-
-
-
-
-; ===========================================================================
-; ----------------------------------------------------------------------------
-; Object 27 - An explosion, giving off an animal and 100 points
-; ----------------------------------------------------------------------------
-; Sprite_21088:
-Obj27:
-	moveq	#0,d0
-	move.b	routine(a0),d0
-	move.w	Obj27_Index(pc,d0.w),d1
-	jmp	Obj27_Index(pc,d1.w)
-; ===========================================================================
-; off_21096: Obj27_States:
-Obj27_Index:	offsetTable
-		offsetTableEntry.w Obj27_InitWithAnimal	; 0
-		offsetTableEntry.w Obj27_Init		; 2
-		offsetTableEntry.w Obj27_Main		; 4
-; ===========================================================================
-; loc_2109C: Obj27_Init:
-Obj27_InitWithAnimal:
-	addq.b	#2,routine(a0) ; => Obj27_Init
-	jsrto	JmpTo2_AllocateObject
-	bne.s	Obj27_Init
-	_move.b	#ObjID_Animal,id(a1) ; load obj28 (Animal and 100 points)
-	move.w	x_pos(a0),x_pos(a1)
-	move.w	y_pos(a0),y_pos(a1)
-	move.w	objoff_3E(a0),objoff_3E(a1)	; Set by Touch_KillEnemy
-
-; loc_210BE: Obj27_Init2:
-Obj27_Init:
-	addq.b	#2,routine(a0) ; => Obj27_Main
-	move.l	#Obj27_MapUnc_21120,mappings(a0)
-	move.w	#make_art_tile(ArtTile_ArtNem_Explosion,0,0),art_tile(a0)
-	jsrto	JmpTo12_Adjust2PArtPointer
-	move.b	#1<<render_flags.level_fg,render_flags(a0)
-	move.b	#1,priority(a0)
-	move.b	#0,collision_flags(a0)
-	move.b	#$C,width_pixels(a0)
-	move.b	#3,anim_frame_duration(a0)
-	move.b	#0,mapping_frame(a0)
-	move.w	#SndID_Explosion,d0
-	jsr	(PlaySound).l
-
-; loc_21102:
-Obj27_Main:
-	subq.b	#1,anim_frame_duration(a0)
-	bpl.s	+
-	move.b	#7,anim_frame_duration(a0)
-	addq.b	#1,mapping_frame(a0)
-	cmpi.b	#5,mapping_frame(a0)
-	beq.w	JmpTo18_DeleteObject
-+
-	jmpto	JmpTo10_DisplaySprite
-; ===========================================================================
-; -------------------------------------------------------------------------------
-; sprite mappings
-; -------------------------------------------------------------------------------
-Obj27_MapUnc_21120:	include "mappings/sprite/obj27.asm"
-
-
-
 
 ; ===========================================================================
 ; ----------------------------------------------------------------------------
